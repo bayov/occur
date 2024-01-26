3c674174a845e500a9340ee30a7d73a328065b5e
Hagai Akibayov <hagai.akibayov@gmail.com>
2024-01-26 21:45:19 +0200

    Fiddle with scylla

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
-fn main() {}
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    color_backtrace::install();
+
+    let _ = scylla::SessionBuilder::new()
+        .known_node("127.0.0.1:9042")
+        .build()
+        .await?;
+
+    Ok(())
+}
